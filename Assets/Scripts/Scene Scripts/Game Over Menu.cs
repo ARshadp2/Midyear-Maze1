9d78a3b96c86eed0d97c9764f0c7b3384eab8dc5
jaleng211 <giangj@bxscience.edu>
2025-01-04 17:13:21 -0500

    Scenes made

---
+++ Assets/Scripts/Scene Scripts/Game Over Menu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu: MonoBehaviour
+{
+    public void OnRestartButton() {
+        SceneManager.LoadScene("TestScene");
+    }
+    public void OnQuitButton() {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Scene Scripts/Menu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    void Update() {
+        Cursor.visible = true;
+    }
+    // Start is called before the first frame update
+    public void OnStartButton() {
+        SceneManager.LoadScene("TestScene");
+    }
+    public void OnQuitButton() {
+        Application.Quit();
+    }
+    public void OnHowToButton() {
+        SceneManager.LoadScene("Instructions Menu");
+    }
+}
